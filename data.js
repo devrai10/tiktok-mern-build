0333077c55ee7ba3dff3434cf2dec78dada1085a
devrai10 <67330685+devrai10@users.noreply.github.com>
2020-09-06 23:18:52 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,25 @@
+const data = [
+  {
+    url:
+      "https://videolinks.com/pub/media/videolinks/video/dji.osmo.action.mp4",
+    channel: "DevR101",
+    description: "Sahara Desert Adventure",
+    song: "Take Me Higher Than The Sky",
+    likes: "123",
+    messages: "456",
+    shares: "779",
+  },
+
+  {
+    url:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    channel: "Animator",
+    description: "Animated Story",
+    song: "Gardens of elixir",
+    likes: "321",
+    messages: "654",
+    shares: "977",
+  },
+];
+
+export default data;
